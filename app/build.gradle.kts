88418c03c402eb1d494ad9c98129d6111433e8b5
MerceMaron <ms.abrego@alumno.etec.um.edu.ar>
2025-10-17 08:17:13 -0300

    AppTest y build.gradle modificados como en la issue 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@
  * For more details on building Java & JVM projects, please refer to https://docs.gradle.org/9.0.0/userguide/building_java_projects.html in the Gradle documentation.
  */
 
-
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.kotlin.jvm)
@@ -19,18 +18,40 @@ repositories {
     mavenCentral()
 }
 
+val javafxVersion = "20"
+
+
 dependencies {
     // Use the Kotlin JUnit 5 integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
     // Use the JUnit 5 integration.
     testImplementation(libs.junit.jupiter.engine)
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    listOf("base", "controls", "fxml", "graphics").forEach { module ->
+        implementation("org.openjfx:javafx-$module:$javafxVersion") {
+            artifact {
+                classifier = "linux"
+            }
+        }
+    }
 }
+
+
+tasks.named<JavaExec>("run") {
+    val osName = "linux"
+    val javafxModules = listOf("javafx.controls", "javafx.fxml")
+
+    jvmArgs = listOf(
+        "--module-path", configurations.runtimeClasspath.get().asPath,
+        "--add-modules", javafxModules.joinToString(",")
+    )
+}
+
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
@@ -44,7 +65,17 @@ application {
     mainClass = "org.example.AppKt"
 }
 
+
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.test {
+    useJUnitPlatform()
+    filter {
+        isFailOnNoMatchingTests = false
+    }
+}
+
+
